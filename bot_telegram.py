83c5c08e8ce1912025a17451fc8ef794e185be6d
Shamil Ramazanov <shamil.ramazanov.1994@mail.ru>
2022-09-05 19:12:36 +0300

    Админка для бота. FSM машина состояний

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,3 +1,4 @@
+'''Файл входа.'''
 from aiogram.utils import executor
 
 from create_bot import dp
@@ -8,6 +9,7 @@ async def on_startup(_):
     print('Бот вышел в онлайн')
 
 client.register_handlers_client(dp)
+admin.register_handlers_admin(dp)
 other.register_handlers_other(dp)
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
--- create_bot.py
+++ create_bot.py
@@ -1,7 +1,12 @@
+'''Файл создания экземпляра бота.'''
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 import os
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
+storage = MemoryStorage()
+
+# Создаем экземпляр бота
 bot = Bot(token=os.getenv('TOKEN'))
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
